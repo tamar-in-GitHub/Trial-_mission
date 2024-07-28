7aa3695992e71c1faa8119607ef3fdf1be454f27
Tamar <T0533169540@gmail.com>
2024-07-28 17:26:54 +0300

    Experimental exercise push

---
+++ Server/DAL/DTO/SettlementsDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DAL.DTO
+{
+    public class SettlementsDto
+    {
+        [Required]
+        public string Name { get; set; }
+        // שדות נוספים
+    }
+}
+
+
+//using System.ComponentModel.DataAnnotations;
+
+//namespace DAL.DTO
+//{
+//    public class SettlementsDto
+//    {
+//        public int Id { get; set; }
+
+//        [Required]
+//        public string Name { get; set; }
+//        // שדות נוספים
+//    }
+//}
